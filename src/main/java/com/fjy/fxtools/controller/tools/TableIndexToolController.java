75f5e27d6a1d1c8ba1d29ad0640274f553dabe44
fangjiayun <fangjiayun@nnuo.com>
2024-09-18 17:39:02 +0800

    [新增功能](javafx-selenium): 开发表索引查询工具

--- src/main/java/com/fjy/fxtools/controller/tools/TableIndexToolController.java
+++ src/main/java/com/fjy/fxtools/controller/tools/TableIndexToolController.java
@@ -3,6 +3,8 @@ package com.fjy.fxtools.controller.tools;
 import com.fjy.fxtools.view.tools.TableIndexToolView;
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @apiNote  首页控制器
@@ -11,10 +13,16 @@ import javafx.fxml.FXML;
 @FXMLController
 public class TableIndexToolController extends TableIndexToolView {
 
-
     @FXML
-    protected void onHelloButtonClick() {
-        String taxNum = taxNumTextField.getText();
-        userRechargeTable.setText("充值记录表：sys_user_recharge_"+ Math.abs(taxNum.hashCode()) % 32);
+    protected void confirmButtonClick() {
+        String keyText = keyTextField.getText();
+        int count = Integer.parseInt(StringUtils.defaultString(countTextField.getText(), "1"));
+        RadioButton selectRadio = (RadioButton)algorithmGroup.getSelectedToggle();
+        int index = -1;
+        switch (selectRadio.getId()){
+            case "hashCode": index = (Math.abs(keyText.hashCode()) % count);break;
+            default:break;
+        }
+        resultTable.setText("所在分表：table_"+ index);
     }
 }
--- src/main/java/com/fjy/fxtools/view/tools/TableIndexToolView.java
+++ src/main/java/com/fjy/fxtools/view/tools/TableIndexToolView.java
@@ -1,8 +1,13 @@
 package com.fjy.fxtools.view.tools;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import lombok.Data;
 
 /**
@@ -12,10 +17,25 @@ import lombok.Data;
  * @date 2024-09-18 09:57
  **/
 @Data
-public abstract class TableIndexToolView {
+public abstract class TableIndexToolView implements Initializable {
 
     @FXML
-    protected Label userRechargeTable;
+    protected Label resultTable;
     @FXML
-    protected TextField taxNumTextField;
+    protected TextField keyTextField;
+    @FXML
+    protected TextField countTextField;
+    @FXML
+    protected RadioButton hashCode;
+    protected ToggleGroup algorithmGroup;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        bindingGroup();
+    }
+
+    private void bindingGroup() {
+        algorithmGroup = new ToggleGroup();
+        hashCode.setToggleGroup(algorithmGroup);
+    }
 }
