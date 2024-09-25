57e2b5a6b43849d52d13998bd734a2acb0797cb7
fangjiayun <fangjiayun@nnuo.com>
2024-09-25 16:32:07 +0800

    [新增功能](javafx-selenium): 开发selenium测试工具

--- src/main/java/com/fjy/fxtools/controller/tools/SeleniumTestToolController.java
+++ src/main/java/com/fjy/fxtools/controller/tools/SeleniumTestToolController.java
@@ -1,6 +1,5 @@
 package com.fjy.fxtools.controller.tools;
 
-import com.fjy.fxtools.enums.BrowserDriverEnum;
 import com.fjy.fxtools.utils.SeleniumUtil;
 import com.fjy.fxtools.utils.thread.ThreadPoolUtil;
 import com.fjy.fxtools.view.tools.SeleniumTestToolView;
@@ -8,6 +7,7 @@ import de.felixroske.jfxsupport.FXMLController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javax.annotation.Resource;
+import org.openqa.selenium.By;
 
 /**
  * @apiNote  selinuim测试
@@ -17,7 +17,7 @@ import javax.annotation.Resource;
 public class SeleniumTestToolController extends SeleniumTestToolView {
 
     @Resource
-    private ThreadPoolUtil<?> threadPoolUtil;
+    private ThreadPoolUtil<String> threadPoolUtil;
 
     @FXML
     protected void confirmButtonClick() {
@@ -25,14 +25,24 @@ public class SeleniumTestToolController extends SeleniumTestToolView {
         loadingImage.setVisible(true);
         rotateTransition.play();
         threadPoolUtil.async(()->{
-            SeleniumUtil.doWebDriverTask(BrowserDriverEnum.chrome,d->{
+            SeleniumUtil.doWebDriverTask(null,false,d->{
                 Platform.runLater(() -> {
                     loadingImage.setVisible(false);
                     rotateTransition.stop();
                 });
-                d.get("https://www.baidu.com");
+                d.get("https://cn.bing.com/");
+                SeleniumUtil.getWebElementByWait(null,By.id("id_s")).click();
+                SeleniumUtil.getWebElementByWait(null,By.className("id_text_signin")).click();
+                SeleniumUtil.getWebElementByWait(null,By.id("i0116")).sendKeys("fjy1071875401@outlook.com");
+                SeleniumUtil.getWebElementByWait(null,By.id("idSIButton9")).submit();
+                SeleniumUtil.getWebElementByWait(null,By.id("i0118")).sendKeys("fjy199202073018");
+                SeleniumUtil.getWebElementByWait(null,By.id("idSIButton9")).submit();
+                SeleniumUtil.getWebElementByWait(null,By.id("acceptButton")).submit();
             });
-        });
-        textValue.setText("success");
+            return "success";
+        },true);
+        threadPoolUtil.get();
+        textValue.setText("success"+SeleniumUtil.getWebElementByWait(null,By.id("id_n")).getText());
+        SeleniumUtil.quitWebDriver();
     }
 }
--- src/main/java/com/fjy/fxtools/utils/SeleniumUtil.java
+++ src/main/java/com/fjy/fxtools/utils/SeleniumUtil.java
@@ -1,11 +1,17 @@
 package com.fjy.fxtools.utils;
 
 import com.fjy.fxtools.enums.BrowserDriverEnum;
+import java.time.Duration;
 import java.util.function.Consumer;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * selenium的工具类
@@ -14,33 +20,69 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * @date 2024-09-24 14:56
  **/
 @Slf4j
+@Data
 public class SeleniumUtil {
 
-    public static WebDriver getWebDriver(BrowserDriverEnum driverEnum){
+    private static WebDriver webDriver;
+
+    public static WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+    /**
+     * 创建新的驱动
+     */
+    public static WebDriver createWebDriver(BrowserDriverEnum driverEnum){
         if (BrowserDriverEnum.chrome == driverEnum){
-            return new ChromeDriver();
+            webDriver = new ChromeDriver();
         }
         else if (BrowserDriverEnum.firefox == driverEnum){
-            return new FirefoxDriver();
+            webDriver = new FirefoxDriver();
+        }else {
+            webDriver = new ChromeDriver();
         }
-        return new ChromeDriver();
+        return webDriver;
     }
 
-    public static void quitWebDriver(WebDriver driver){
-        if (driver != null){
-            driver.quit();
+    /**
+     * 退出驱动
+     */
+    public static void quitWebDriver(){
+        if (webDriver != null){
+            webDriver.quit();
+        }
+    }
+    public static void closeWebDriver(){
+        if (webDriver != null){
+            webDriver.close();
         }
     }
 
-    public static void doWebDriverTask(BrowserDriverEnum driverEnum, Consumer<WebDriver> consumer){
-        WebDriver driver = null;
+    /**
+     * 执行浏览器操作
+     */
+    public static void doWebDriverTask(BrowserDriverEnum driverEnum,Boolean ifQuit, Consumer<WebDriver> consumer){
+        WebDriver driver;
         try {
-            driver = SeleniumUtil.getWebDriver(driverEnum);
+            driver = SeleniumUtil.createWebDriver(driverEnum);
             consumer.accept(driver);
         } catch (Exception e) {
             log.error("处理selenium脚本异常",e);
         }finally {
-            SeleniumUtil.quitWebDriver(driver);
+            if (ifQuit==null || ifQuit){
+                SeleniumUtil.quitWebDriver();
+            }
         }
     }
+
+    /**
+     * 使用等待的方式获取页面元素
+     * @param seconds 最大等待时间，默认5s
+     * @param by 页面元素
+     */
+    public static WebElement getWebElementByWait(Long seconds,By by){
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(seconds==null?10:seconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
 }
